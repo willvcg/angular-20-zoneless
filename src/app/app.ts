e931c2339ba49de5b65e8e9c6785fe4b8e8b5c31
Will Cumbicus <wilmanvc93@gmail.com>
2025-11-01 18:06:35 +0100

    Test for zoneless

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,34 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { JsonPipe } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, effect, inject, signal } from '@angular/core';
+import { tap } from 'rxjs';
+
+export interface Post {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [JsonPipe],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
+  private readonly http = inject(HttpClient);
+  private readonly _loadPosts = this._loadPostsEffect();
   protected readonly title = signal('angular-20-zoneless');
+  protected posts = signal<Post[]>([]);
+
+  private _loadPostsEffect() {
+    effect(() => {
+      this.http.get<Post[]>('https://jsonplaceholder.typicode.com/posts').pipe(
+        tap((posts) => {
+          this.posts.set(posts);
+        })
+      ).subscribe();
+    });
+  }
 }
